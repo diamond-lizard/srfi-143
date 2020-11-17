565a27181a968534a89ef46d58907f790e73524a
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-17 13:21:23 -0800

    Using srfi-143/srfi-143.scm instead of srfi-143/rubber-chicken.scm

--- srfi-143.scm
+++ srfi-143.scm
@@ -1,23 +1,6 @@
-(module srfi-143 ()
-  (import scheme)
-  (import (except (chicken base) fixnum?))
-  (import (chicken module))
-  (import (chicken platform))
-  (import (chicken module))
+(import scheme)
+(import (chicken platform))
 
-  (export fx-width fx-greatest fx-least)
-  (export fixnum? fx=? fx<? fx>? fx<=? fx>=?
-          fxzero? fxpositive? fxnegative?
-          fxodd? fxeven? fxmax fxmin)
-  (export fx+ fx- fxneg fx* fxquotient fxremainder
-          fxabs fxsquare fxsqrt)
-  (export fx+/carry fx-/carry fx*/carry)
-  (export fxnot fxand fxior fxxor fxarithmetic-shift
-          fxarithmetic-shift-left fxarithmetic-shift-right
-          fxbit-count fxlength fxif fxbit-set? fxcopy-bit
-          fxfirst-set-bit fxbit-field
-          fxbit-field-rotate fxbit-field-reverse)
+(register-feature! 'srfi-143)
 
-  (register-feature! 'srfi-143)
-
-  (include "srfi-143/rubber-chicken.scm"))
+(include "srfi-143/srfi-143.scm")
